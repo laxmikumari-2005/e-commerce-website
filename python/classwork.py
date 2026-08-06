30444adae9e5e5ad385847f8d5ff1955e4f8bdc4
Laxmi Kumari <lk729380@gmail.com>
2026-08-06 12:37:32 +0530

    Add files via upload

---
+++ python/classwork.py
@@ -0,0 +1,61 @@
+
+#variable
+name="Laxmi Kumari"
+print(name)
+
+name="Tony Stark"
+print(type(name))
+
+#type function
+x=100
+print(type(x))
+
+x=[1,2,3,4]
+print(type(x))
+x=["a","b","c"]
+print(type(x))
+
+
+##x=input("enter value:")
+#print(x)
+
+#function
+def fun():
+    print("hello coders")
+    
+fun()
+    
+print("""Learning path:
+      -python basics
+      -data science
+      -AI/ML""")
+
+#input function
+#x=input("enter value:")
+#print(x)
+#x=int(input("enter value:"))
+#print(x)
+
+#method
+text="Laxmi"
+print(text.lower())    
+
+num=99
+print(num.bit_length())   #attribute error
+
+#fstring
+name="Laxmi"
+print(f"My name is {name}")
+x="code"
+print(f"{name} learns to {x}")
+
+#special characters
+print("https://chatgpt.com/c/6a72ddb0-4564-83ee-a384-d521e50649c0: c:\Users")   #unicode error
+
+print("she is a \"good girl\"")
+
+#type change
+name="Laxmi"
+age=21
+print(type(name))
+print("age:"+str(age))
